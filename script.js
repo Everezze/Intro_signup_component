d5e8c8b9b84443411b535f6ca1d9a7206c1d19c1
omarsalim <83906211+Everezze@users.noreply.github.com>
2024-01-20 17:46:01 +0100

    most input checks done, added style class to show error elements

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ const inputs = Array.from(document.querySelectorAll("input"));
 const submit = document.querySelector("button");
 const form = document.querySelector("form");
 let submittedOnce = false;
-let invalidForm = false;
+let inputValidation = [false,false,false,false];
 
-//console.log(inputs[inputs.length-1]);
-//console.log(submit);
-//console.log(form);
-//const regexNames = /^\s\w/;
-//const regexEmail = /^s\w@\w\.[a-z]{2,3}/;
-//const regexPassword = /^s\w{5,13}/;
 
 const regexes = {
     "first-name": /^\w+$/,
@@ -18,66 +12,59 @@ const regexes = {
     "password": /^\w{5,13}$/ 
 }
 
-//console.log(regexes[inputs[3].getAttribute("name")])
-
 inputs.forEach(function(element,index){
     element.addEventListener("input",function(){
-        if(submittedOnce){
-            let iconError = this.nextElementSibling;
-            let msgError = this.parentNode.nextElementSibling;
-            if(this.value.match(regexes[this.getAttribute("name")]) || this.value == ""){
+        //console.log(this);
+        if(this.value.match(regexes[this.getAttribute("name")]) || this.value == ""){
+            console.log("valid input");
+            if(submittedOnce){
+                let iconError = this.nextElementSibling;
+                let msgError = this.parentNode.nextElementSibling;
                 iconError.classList.remove("active");
                 msgError.classList.remove("active");
             }
+            if(this.value == ""){
+                inputValidation[index] = false;
+            }
             else{
-                if(!iconError.classList.contains("active")){
+                inputValidation[index] = true;
+            }
+        }
+
+        else{
+            let iconError = this.nextElementSibling;
+            let msgError = this.parentNode.nextElementSibling;
+            console.log("INvalid input");
+            if(!iconError.classList.contains("active")){
+                if(submittedOnce){
                     iconError.classList.add("active");
                     msgError.classList.add("active");
                 }
+            inputValidation[index] = false;
             }
         }
     });
 });
 
 submit.addEventListener("click",function(event){
-    //if(!submittedOnce){
-    //    inputs.every(function(element,index){
-    //        let iconError = element.nextElementSibling;
-    //        let msgError = element.parentNode.nextElementSibling;
-    //        if(!element.value.match(regexes[element.getAttribute("name")])){
-    //            iconError.classList.add("active");
-    //            msgError.classList.add("active");
-    //            if(!invalidForm){
-    //                invalidForm = true;
-    //            }
-    //        }
-    //    });
-    //    if(invalidForm){
-    //        event.preventDefault();
-    //    }
-    //}
-    //
-    //
-    //let invalidForm = inputs.some(function(element,index){
-    //    let iconError = element.nextElementSibling;
-    //    let msgError = element.parentNode.nextElementSibling;
-    //    if(!element.value.match(regexes[element.getAttribute("name")])){
-    //        iconError.classList.add("active");
-    //        msgError.classList.add("active");
-    //    }
-    //    return element.classList.contains("active");
-    //});
-    //console.log(invalidForm);
-
-    //if(invalidForm){
-    //    event.preventDefault();
-    //}
-    //
-    inputs.forEach(function(element,index){
-        console.log(element);
-        console.log(typeof element.value);
-        console.log(element.value.match(regexes[element.getAttribute("name")]));
-    });
-
-    event.preventDefault();
+    let sendableForm = inputValidation.every( element => element == true);
+    if(!submittedOnce){
+        inputValidation.forEach(function(element,index){
+            if(!element){
+                let input = inputs[index];
+                let iconError = input.nextElementSibling;
+                let msgError = input.parentNode.nextElementSibling;
+                iconError.classList.add("active");
+                msgError.classList.add("active");
+            }
+        });
+        submittedOnce=true;
+    }
+    if(!sendableForm){
+        event.preventDefault();
+    }
 });
+
+window.addEventListener("load",function(){
+    form.reset();
+})
